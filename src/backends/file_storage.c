7a6b3278df9b88ea5f55a486f89f79f4f54c1f46
M4xw <m4x@m4xw.net>
2019-02-04 14:11:08 +0100

    [WIP] Add mupen64plus-core minimal changes
    Platform buildfix - is this accurate enough?
    
    WIP Dynarec
    
    Speed workaround at perf cost
    
    C89 fix
    
    Remove dummy_rsp
    
    Fix awk
    
    Fix savestates, some ground work for cheats
    
    cleanup
    
    Transition fixes (CBFD etc)
    
    Fix cheats
    
    Fix Savefiles
    
    [LIBNX] Small dynarec perf hack
    
    ASM defines for android, currently not generated.
    
    Libnx transition cleanup

--- src/backends/file_storage.c
+++ src/backends/file_storage.c
@@ -90,8 +90,10 @@ static size_t file_storage_size(const void* storage)
 
 static void file_storage_save(void* storage)
 {
-    if (netplay_is_init() && netplay_get_controller(0) == -1)
-        return;
+    //TODO: Fix storage
+    //if (netplay_is_init() && netplay_get_controller(0) == -1)
+    //    return;
+    return;
 
     struct file_storage* fstorage = (struct file_storage*)storage;
 
@@ -116,6 +118,9 @@ static void file_storage_parent_save(void* storage)
 
 static void file_storage_dd_sdk_dump_save(void* storage)
 {
+    //TODO: Fix storage
+    return;
+    
     static uint8_t sdk_buffer[SDK_FORMAT_DUMP_SIZE];
     struct file_storage* fstorage = (struct file_storage*)storage;
 
